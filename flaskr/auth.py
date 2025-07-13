888f826598082b4e66dbfa94a956195e7439adbf
Jan Malmsten <malmstenj@outlook.com>
2025-07-13 08:04:27 +0300

    Added few modifications, added CSS file and modified the README file

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -32,7 +32,7 @@ def register():
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
-                return redirect(url_for("auht.login"))
+                return redirect(url_for("auth.login"))
             
         flash(error)
 
